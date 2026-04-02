9765aa70d252b6a293b6081f11d0e7833826e9fb
Rahul <rahulmjoshi0007@gmail.com>
2026-04-02 11:50:36 +0530

    Added Docker setup + full stack project

--- construction-backend/server.js
+++ construction-backend/server.js
@@ -2,9 +2,17 @@ require("dotenv").config(); // 🔥 load env FIRST
 
 const app = require("./src/app");
 const sequelize = require("./src/config/db");
+const client = require('prom-client');
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
 
 const PORT = process.env.PORT || 5000;
+collectDefaultMetrics();
 
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 /* =========================================
    CONNECT MYSQL & START SERVER
 ========================================= */
@@ -20,3 +28,5 @@ sequelize.sync()
   .catch((err) => {
     console.error("Database connection failed:", err);
   });
+
+  
